f7e78a1b65be16fbb68c83844c70b93a72e8a168
Xak Biakov <k2annicet@gmail.com>
2023-06-01 13:31:58 +0000

    Update

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
 import AppLayout from '@/layout/AppLayout.vue';
-
 const router = createRouter({
     history: createWebHashHistory(),
     routes: [
